71ead328bf9e96ed39230472aeff3789ed5da6c7
Jhonatan I <jhonatan-nascimento94@hotmail.com>
2019-10-15 11:06:52 -0300

    Private routes

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const DashBoard = () => {
-  return (
-    <div>
-
-    </div>
-  );
+  return <h1>DASHBOARD</h1>
 };
 
 export default DashBoard;
--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const SignIn = () => {
-  return (
-    <div>
-
-    </div>
-  );
+  return <h1>LOGIN</h1>;
 };
 
 export default SignIn;
