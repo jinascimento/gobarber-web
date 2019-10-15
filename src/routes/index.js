442a226e0780d9c255de10238c8e36d063eb93f2
Jhonatan I <jhonatan-nascimento94@hotmail.com>
2019-10-15 10:44:14 -0300

    Add reactotron

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import SignIn from '../pages/Signin';
+import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 
 import Profile from '../pages/Profile';
