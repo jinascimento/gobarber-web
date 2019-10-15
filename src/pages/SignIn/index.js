2d9872382e97642346f6e2b92205d8fde9246692
Jhonatan I <jhonatan-nascimento94@hotmail.com>
2019-10-15 12:02:21 -0300

    Add style to SignUp/SignIn pages

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo.svg';
 
 const SignIn = () => {
-  return <h1>LOGIN</h1>;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <form>
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </form>
+    </>
+  );
 };
 
 export default SignIn;
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo.svg';
 
 const SignUp = () => {
   return (
-    <div>
+    <>
+      <img src={logo} alt="GoBarber" />
+      <form>
+        <input type="text" placeholder="Nome completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
 
-    </div>
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho uma conta</Link>
+      </form>
+    </>
   );
 };
 
