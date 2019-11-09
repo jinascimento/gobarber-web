fb84a011a563b4aaec2f1c2de193cff383b59eaf
Jhonatan I <jhonatan-nascimento94@hotmail.com>
2019-11-09 16:28:39 -0300

    Error notification when invalid login

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,28 +1,33 @@
 import { all, takeLatest, call, put } from 'redux-saga/effects';
-
+import { toast } from 'react-toastify';
 import history from '~/services/history';
 import api from '~/services/api';
 
-import { signInSucess } from './actions';
+import { signFailure, signInSucess } from './actions';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  const response = yield call(api.post, 'sessions', {
-    email,
-    password,
-  });
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
 
-  const { token, user } = response.data;
+    const { token, user } = response.data;
 
-  if (!user.provider) {
-    console.tron.error('Usuário não é prestador');
-    return;
+    if (!user.provider) {
+      toast.error('Usuário não é prestador');
+      return;
+    }
+
+    yield put(signInSucess(token, user));
+
+    history.push('/dashboard');
+  } catch (err) {
+    toast.error('Falha na autenticação, verifique seus dados');
+    yield put(signFailure());
   }
-
-  yield put(signInSucess(token, user));
-
-  history.push('/dashboard');
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
