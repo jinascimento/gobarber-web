cc5134c513a003d63d308f957950b328808f4a83
Jhonatan I <jhonatan-nascimento94@hotmail.com>
2019-11-28 10:08:16 -0300

    SignOut

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import logoPurple from '../../assets/logo_purple.svg';
 import { Container, Content, Profile } from './styles';
 import Notifications from '../Notifications/index';
 
 export default function Header() {
+  const profile = useSelector(state => state.user.profile);
+
   return (
     <Container>
       <Content>
